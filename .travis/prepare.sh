d62380cf43985d4033378b6622ee70f9f8475742
Lorenzo <fontanalorenzo@me.com>
2014-08-28 21:39:39 +0200

    Initial Travis

---
+++ .travis/prepare.sh
@@ -0,0 +1,11 @@
+apt-get install git-svn
+
+echo "Cloning CLD2"
+git svn clone http://cld2.googlecode.com/svn/trunk/ libcld2
+
+echo "Compiling libcld2"
+cd libcld2/internal
+./compile_libs.sh
+
+echo "Installing libcld2"
+cp libcld2.so /usr/local/lib
